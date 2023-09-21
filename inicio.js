031dbe4e114fe47584b257b30ac3d19f298e7cae
AdrianS100304 <jose13699631@gmail.com>
2023-09-20 23:10:40 -0600

    Implementacion de inicio y registro

---
+++ inicio.js
@@ -0,0 +1,140 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.Image} source={require('./img/img-fondo.jpg')}></Image>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Whatsapp</Text>
+      </View>
+
+      <View style={styles.mainContainer}>
+
+        <View style={styles.centerText}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Correo electronico"
+          />
+          <TextInput
+            style={styles.inputText}
+            placeholder="Contraseña"
+          />
+        </View>
+
+        <View style={styles.TextRight}>
+          <Text style={styles.textOlvContraseña}>Olvidaste tu contraseña?</Text>
+        </View>
+
+        <View style={styles.centerText}>
+          <TouchableOpacity style={styles.personalizarButton}>
+            <Text style={styles.textButton}>LOGIN</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.separacion}>
+          <Text>
+            No tienes cuenta?
+            <Text style={styles.textRegister}> Registrate</Text>
+          </Text>
+        </View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  TextRight: {
+    width: "100%",
+    height: 60,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  Image: {
+    width: "100%",
+    height: 310,
+    position: "absolute",
+  },
+  titleContainer: {
+    width: "100%",
+    height: "30%",
+    alignItems: "center",
+    paddingTop: 140,
+    position: "absolute",
+  },
+  title: {
+    position: "relative",
+    color: "#fff",
+    fontSize: 50,
+  },
+  mainContainer: {
+    backgroundColor: '#fffeaa',
+    marginTop: 275,
+    width: "100%",
+    height: "65%",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    display: "flex",
+    paddingTop: 25,
+  },
+
+  endContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+
+  inputText: {
+    width: 300,
+    height: 40,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    paddingLeft: 10,
+    marginTop: 18,
+    marginBottom: 0,
+  },
+
+  personalizarButton: {
+    backgroundColor: "green",
+    width: "80%",
+    height: 70,
+    borderRadius: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textButton: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  textOlvContraseña: {
+    fontSize: 16,
+    paddingRight: 40,
+  },
+  textRegister: {
+    fontSize: 16,
+    paddingRight: 15,
+    color: "#00BFFF",
+  },
+  separacion: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 180,
+  }
+});
---
+++ registro.js
@@ -0,0 +1,120 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.mainContainer}>
+        <AntDesign style={styles.icono} name="arrowleft" size={30} color="black" />
+        <Text style={styles.title}>
+          Registrate para una {'\n'}nueva cuenta</Text>
+        <Text style={styles.text}>Proporcionanos tu información.</Text>
+
+      </View>
+      <View style={styles.centerText}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Nombre"
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Correo electronico"
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Contraseña"
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Confirmar contraseña"
+        />
+        <View style={styles.centerText}>
+          <TouchableOpacity style={styles.personalizarButton}>
+            <Text style={styles.textButton}>LOGIN</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.separacion}>
+          <Text>
+            Tienes una cuenta?
+            <Text style={styles.textIniciarSesion}> Inicia sesión</Text>
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  mainContainer: {
+    width: "100%",
+    alignItems: "flex-start",
+    paddingTop: 60,
+    position: "relative",
+
+  },
+  title: {
+    position: "relative",
+    color: "#000",
+    fontWeight: "bold",
+    fontSize: 28,
+    paddingLeft: 28,
+  },
+  text: {
+    paddingTop: 15,
+    paddingLeft: 28,
+  },
+  icono: {
+    paddingLeft: 16,
+    paddingBottom: 20,
+  },
+  inputText: {
+    width: 300,
+    height: 40,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    paddingLeft: 10,
+    marginTop: 25,
+    marginBottom: 0,
+  },
+  personalizarButton: {
+    backgroundColor: "green",
+    width: 300,
+    height: 70,
+    borderRadius: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  textButton: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  textIniciarSesion: {
+    fontSize: 16,
+    paddingRight: 15,
+    color: "#00BFFF",
+  },
+  separacion: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 150,
+  }
+});
